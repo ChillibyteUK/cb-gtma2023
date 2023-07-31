e65a185e3351728fc59c99553d76a2aa990dce85
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-07-31 14:12:38 +0100

    button btn-blue

--- single-scs.php
+++ single-scs.php
@@ -7,7 +7,7 @@ $img = get_the_post_thumbnail_url(get_the_ID(),'full');
 $file = get_field('pdf',get_the_ID());
 $fs = filesize( get_attached_file( $file ) );
 
-$fname = "GTMA Supply Chain Solutions - " . get_the_title();
+$fname = "GTMA Supply Chain Solutions - " . get_the_title() . ".pdf";
 
 ?>
 <main id="main" class="scs-single">
@@ -21,7 +21,7 @@ $fname = "GTMA Supply Chain Solutions - " . get_the_title();
     <div class="container-xl">
         <?=do_shortcode('[dearpdf id="303"][/dearpdf]')?>
         <div class="my-5">
-        <a href="<?=get_the_permalink(get_the_ID())?>" download="<?=$fname?> class="btn btn-primary">Download PDF (<?=formatBytes($fs)?>)</a>
+        <a href="<?=get_the_permalink(get_the_ID())?>" download="<?=$fname?>" class="btn btn-blue">Download PDF (<?=formatBytes($fs)?>)</a>
         </div>
     </div>
 </main>
