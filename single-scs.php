087cf045d1f89315f3852d2f3685d443f63511ac
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-07-31 14:10:36 +0100

    Update single-scs.php

--- single-scs.php
+++ single-scs.php
@@ -3,6 +3,12 @@
 defined('ABSPATH') || exit;
 get_header();
 $img = get_the_post_thumbnail_url(get_the_ID(),'full');
+
+$file = get_field('pdf',get_the_ID());
+$fs = filesize( get_attached_file( $file ) );
+
+$fname = "GTMA Supply Chain Solutions - " . get_the_title();
+
 ?>
 <main id="main" class="scs-single">
     <section class="breadcrumbs container-xl">
@@ -14,18 +20,9 @@ $img = get_the_post_thumbnail_url(get_the_ID(),'full');
     </section>
     <div class="container-xl">
         <?=do_shortcode('[dearpdf id="303"][/dearpdf]')?>
-
-        Download
-        <?php
-
-$file = get_field('pdf',get_the_ID());
-$fs = filesize( get_attached_file( $file ) );
-
-echo $fs;
-
-
-
-        ?>
+        <div class="my-5">
+        <a href="<?=get_the_permalink(get_the_ID())?>" download="<?=$fname?> class="btn btn-primary">Download PDF (<?=formatBytes($fs)?>)</a>
+        </div>
     </div>
 </main>
 <?php
