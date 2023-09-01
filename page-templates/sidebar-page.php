0acc975914fab83d66ef020a8023910484c815bb
CheechRockwizard <github@lamcat.co.uk>
2023-09-01 14:12:22 +0100

    sidebar shenanigans

--- page-templates/sidebar-page.php
+++ page-templates/sidebar-page.php
@@ -4,6 +4,13 @@ Template Name: Sidebar with Siblings
 */
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
+
+add_action('wp_head', function () {
+    ?>
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<?php
+});
+
 get_header();
 
 $me = get_the_ID();
@@ -36,11 +43,12 @@ if ($class == 'partner') {
                 the_content();
 ?>
             </div>
-            <div class="col-md-4">
-                <div class="partner__sidebar mb-4">
-                    <h3><?=get_field('sidebar_title')?>
-                    </h3>
-                    <?php
+            <div class="col-md-4 sidebar">
+                <div class="sidebar__inner">
+                    <div class="sidebar__siblings mb-4">
+                        <h3><?=get_field('sidebar_title')?>
+                        </h3>
+                        <?php
 $sibs = new WP_Query(array(
     'post_type'      => 'page',
     'posts_per_page' => -1,
@@ -54,20 +62,37 @@ if ($sibs->have_posts()) {
         $sibs->the_post();
         $active = get_the_ID() == $me ? 'active' : '';
         ?>
-                    <li><a class="<?=$active?>"
-                            href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
-                    </li>
-                    <?php
+                        <li><a class="<?=$active?>"
+                                href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
+                        </li>
+                        <?php
     }
     echo '</ul>';
 }
 wp_reset_postdata();
 ?>
+                    </div>
+                    <div class="sidebar__search">
+                        <div class="h4 mb-3">Search for Products &amp; Services</div>
+                        <div class="form">
+                            <input class="form-control" type="text" id="searchInput" autocomplete="off">
+                            <button id="go" class="btn-search">Search</button>
+                            <input type="hidden" name="source" id="sourceInput">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </main>
 <?php
+add_action('wp_footer', function () {
+    ?>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<script src="<?=get_stylesheet_directory_uri()?>/js/search.js"></script>
+<?php
+}, 9999);
+
 get_footer();
 ?>
