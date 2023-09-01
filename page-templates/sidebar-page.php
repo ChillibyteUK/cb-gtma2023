14f479a4e2b1333dd4bcb5bb02eb217ddd6634aa
CheechRockwizard <github@lamcat.co.uk>
2023-09-01 13:48:14 +0100

    sidebar / partner pages

--- page-templates/sidebar-page.php
+++ page-templates/sidebar-page.php
@@ -5,15 +5,34 @@ Template Name: Sidebar with Siblings
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 get_header();
+
+$me = get_the_ID();
+$parent = wp_get_post_parent_id($me);
+
+$business = get_page_by_path('business-partners');
+$services = get_page_by_path('services');
+
+$class = '';
+if ($parent == $business->ID) {
+    $class = 'partner';
+}
+if ($parent == $services->ID) {
+    $class = 'service';
+}
+
 ?>
-<main id="main" class="partner">
+<main id="main" class="<?=$class?>">
     <div class="container-xl">
         <h1><?=get_the_title()?></h1>
         <div class="row">
             <div class="col-md-8">
+                <?php
+if ($class == 'partner') {
+    ?>
                 <img class="partner__logo"
                     src="<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>">
                 <?php
+}
                 the_content();
 ?>
             </div>
@@ -22,8 +41,6 @@ get_header();
                     <h3><?=get_field('sidebar_title')?>
                     </h3>
                     <?php
-                $me = get_the_ID();
-$parent = wp_get_post_parent_id($me);
 $sibs = new WP_Query(array(
     'post_type'      => 'page',
     'posts_per_page' => -1,
