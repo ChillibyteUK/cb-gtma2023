14509ddb695ddd6287ca19412597c54abb572419
CheechRockwizard <github@lamcat.co.uk>
2023-09-01 10:28:18 +0100

    Update cb_child_pages.php

--- page-templates/blocks/cb_child_pages.php
+++ page-templates/blocks/cb_child_pages.php
@@ -6,9 +6,9 @@
             echo '<h2>' . get_field('page_children__title') . '</h2>';
         }
         ?>
-        <div class="row g-4 justify-content-center">
-<?php
-$children = new WP_Query( array(
+        <div class="services__grid">
+            <?php
+$children = new WP_Query(array(
     'post_type'      => 'page',
     'posts_per_page' => -1,
     'post_parent'    => get_the_ID(),
@@ -16,18 +16,25 @@ $children = new WP_Query( array(
     'orderby'        => 'menu_order'
 ));
 
-if ( $children->have_posts() ) {
-    while ( $children->have_posts() ) {
-        $children->the_post();
-        ?>
-        <div class="col-md-4 col-xl-3">
-            <a class="child_pages__card" href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
-        </div>
-        <?php
-    }
-}
+        if ($children->have_posts()) {
+            while ($children->have_posts()) {
+                $children->the_post();
+                $img = get_the_post_thumbnail_url(get_the_ID(), 'large');
+                $title = get_the_title();
+    
+                ?>
+            <a class="services__card"
+                href="<?=get_the_permalink()?>">
+                <img src="<?=$img?>" alt="">
+                <div class="services__title">
+                    <?=$title?>
+                </div>
+            </a>
+            <?php
+            }
+        }
 
-wp_reset_postdata();
-?>
-    </div>
+        wp_reset_postdata();
+        ?>
+        </div>
 </section>
