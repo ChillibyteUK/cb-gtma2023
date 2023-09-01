8074372bdaadb7e19ff8289cd1389f5a2143379e
CheechRockwizard <github@lamcat.co.uk>
2023-09-01 11:32:13 +0100

    business partner template

---
+++ page-templates/business-partner.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Template Name: Business Parter
+*/
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+get_header();
+?>
+<main id="main" class="partner">
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-md-8">
+                <?php
+                the_content();
+?>
+            </div>
+            <div class="col-md-4">
+                <div class="partner__sidebar">
+                    <h3>GTMA Business Partners</h3>
+                    <?php
+                $me = get_the_ID();
+$parent = wp_get_post_parent_id($me);
+$sibs = new WP_Query(array(
+    'post_type'      => 'page',
+    'posts_per_page' => -1,
+    'post_parent'    => $parent,
+    'order'          => 'ASC',
+    'orderby'        => 'menu_order'
+));
+if ($sibs->have_posts()) {
+    echo '<ul>';
+    while ($sibs->have_posts()) {
+        $sibs->the_post();
+        $active = get_the_ID() == $me ? 'active' : '';
+        ?>
+                    <li><a class="<?=$active?>"
+                            href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
+                    </li>
+                    <?php
+    }
+    echo '</ul>';
+}
+wp_reset_postdata();
+?>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php
+get_footer();
+?>
