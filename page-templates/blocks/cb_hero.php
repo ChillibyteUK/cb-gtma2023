7f4f1121620842e11c72ade0349a2e706c059e92
CheechRockwizard <github@lamcat.co.uk>
2023-09-01 15:13:15 +0100

    Update cb_hero.php

--- page-templates/blocks/cb_hero.php
+++ page-templates/blocks/cb_hero.php
@@ -1,11 +1,12 @@
 <?php
 $class = $block['className'] ?? null ?: '';
 $img = get_the_post_thumbnail_url(get_the_ID(), 'full');
+$title = get_field('alt_title') ?: get_the_title();
 ?>
 <link rel="preload" as="image" href="<?=$img?>">
-<div class="container-xl">
+<div class="container-xl mb-4">
     <header class="hero <?=$class?>"
         style="background-image:url(<?=$img?>)">
-        <h1 class="hero__title"><?=get_the_title()?></h1>
+        <h1 class="hero__title"><?=$title?></h1>
     </header>
 </div>
