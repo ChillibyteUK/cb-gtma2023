bcd24ecaccfdcce75508e766a79fe44e4fdd161d
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-09-04 11:50:19 +0100

    Update cb_contact.php

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -1,6 +1,6 @@
 <section class="contact">
     <div class="container-xl">
-        <div class="row">
+        <div class="row mb-4">
             <div class="col-md-6">
                 <h3>Address</h3>
                 <ul class="fa-ul">
@@ -9,7 +9,7 @@
 
                 <h3>Telephone</h3>
                 <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span> <a href="tel:<?=parse_phone(get_field('contact_phone','options'))?>"><?=get_field('contact_phone','options')?></a></li>
+                    <li><span class="fa-li"><i class="fa-solid fa-phone"></i></span> <a href="tel:<?=parse_phone(get_field('contact_phone','options'))?>"><?=get_field('contact_phone','options')?></a></li>
                 </ul>
 
                 <h3>Email</h3>
@@ -20,7 +20,7 @@
                 <p>If you have any comments or suggestions regarding any part of our web site or have a general enquiry for the Association please leave your contact details in the form.</p>
             </div>
             <div class="col-md-6">
-                FORM
+                <?=do_shortcode('[gravityform id="' . get_field('contact_form_id','options') . '" title="false"]')?>
             </div>
         </div>
         <iframe src="<?=get_field('google_maps_src','options')?>" width="100%" height="400" frameborder="0"></iframe>
