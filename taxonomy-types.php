6a4aa1d6de51835b015d505e3d3a33e3f817f03f
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-09-29 11:19:23 +0100

    Update taxonomy-types.php

--- taxonomy-types.php
+++ taxonomy-types.php
@@ -55,14 +55,7 @@ while ($q->have_posts()) {
     ?>
             <a class="suppliers__card <?=$county_class?>"
                 href="<?=get_the_permalink()?>">
-                <?php
-                echo '<pre>' . get_field('is_featured') . '</pre>';
-                if (get_field('is_featured') == 'Yes') {
-                    ?>
                 <i class="fa-solid fa-star"></i>
-                    <?php
-                }
-                ?>
                 <?=get_the_title()?>
                 <?php
                 if ($county) {
@@ -106,13 +99,6 @@ while ($q->have_posts()) {
     ?>
             <a class="suppliers__card <?=$county_class?>"
                 href="<?=get_the_permalink()?>">
-                <?php
-                if (get_field('is_featured')) {
-                    ?>
-                <i class="fa-solid fa-star"></i>
-                    <?php
-                }
-                ?>
                 <?=get_the_title()?>
                 <?php
                 if ($county) {
