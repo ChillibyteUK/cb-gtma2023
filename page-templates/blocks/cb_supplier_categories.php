becb8faf1db8d9ff857cd0fdfa071175d0a3916d
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-09-29 14:04:50 +0100

    Update cb_supplier_categories.php

--- page-templates/blocks/cb_supplier_categories.php
+++ page-templates/blocks/cb_supplier_categories.php
@@ -1,11 +1,11 @@
 <section class="suppliers py-5">
     <div class="container-xl">
         <?php
-        $terms = get_terms(array('taxonomy' => 'types', 'hide_empty' => false ));
+        $terms = get_terms(array('taxonomy' => 'types', 'hide_empty' => true ));
         foreach ($terms as $t) {
             ?>
         <li><a
-                href="<?=get_term_link($t)?>"><?=$t->name?></a>
+                href="<?=get_term_link($t)?>"><?=$t->name?> (<?=$t->count?>)</a>
         </li>
         <?php
         }
