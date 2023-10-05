3c2ba70b1b28cebb2c588e0c642570acda3f249a
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-10-05 10:59:03 +0100

    Update single-scs.php

--- single-scs.php
+++ single-scs.php
@@ -22,7 +22,7 @@ $fname = "GTMA Supply Chain Solutions - " . get_the_title() . ".pdf";
         <h1><?=get_the_title()?></h1>
         <?php the_content(); ?>
         <?=do_shortcode('[dearpdf id="' . get_field('dearpdf_id') . '"][/dearpdf]')?>
-        <div class="my-5">
+        <div class="my-5 text-center">
             <a href="<?=get_the_permalink(get_the_ID())?>" download="<?=$fname?>" class="btn btn-blue">Download PDF (<?=formatBytes($fs)?>)</a>
         </div>
     </div>
