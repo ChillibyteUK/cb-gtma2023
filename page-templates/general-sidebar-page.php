f98706d3b6863858c157651fbea956a8d9d82678
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-10-10 16:46:36 +0100

    sidebar nonsense

--- page-templates/general-sidebar-page.php
+++ page-templates/general-sidebar-page.php
@@ -1,67 +1,88 @@
 <?php
 /*
-Template Name: Sidebar Page
+Template Name: General Sidebar Page
 */
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 
+$class = $block['className'] ?? null;
+
 get_header();
+global $wp;
+$current_url = home_url( add_query_arg( array(), $wp->request ) ) . '/';
 ?>
 <main id="main" class="<?=$class?>">
     <div class="container-xl">
-        <h1><?=get_the_title()?></h1>
         <div class="row">
             <div class="col-md-8">
-
+                <h1><?=get_the_title()?></h1>
+                <?=get_the_content()?>
             </div>
             <div class="col-md-4">
-                <div class="sidebar">
-                    <div class="sidebar__blue">
-                        <h3>Services Available</h3>
-                        Here are some of the services available to members
-                        <?php
-                        $parent = 134;
-                        $sibs = new WP_Query(array(
-                            'post_type'      => 'page',
-                            'posts_per_page' => -1,
-                            'post_parent'    => $parent,
-                            'order'          => 'ASC',
-                            'orderby'        => 'title'
-                        ));
-                        ?>
-                        <ul>
+                <div class="gsidebar pb-4">
+                    <div class="gsidebar__red mb-2">
+                        <div class="h4">Membership</div>
+                        <div class="gsidebar__inner">
                             <?php
-                            while($sibs->have_posts()) {
-                                $sibs->the_post();
-                                ?>
-                            <li><a href="<?=get_the_permalink()?>"><?=get_the_title()?></a></li>
-                                <?php
-                            }
+                            $parent = 362;
+                            $sibs = new WP_Query(array(
+                                'post_type'      => 'page',
+                                'posts_per_page' => -1,
+                                'post_parent'    => $parent,
+                                'order'          => 'ASC',
+                                'orderby'        => 'title'
+                            ));
+                            $active = '';
                             ?>
-                        </ul>
+                            <ul>
+                                <?php
+                                while($sibs->have_posts()) {
+                                    $sibs->the_post();
+                                    $active = get_the_permalink() == $current_url ? 'active' : '';
+                                    ?>
+                                <li><a class="<?=$active?>" href="<?=get_the_permalink()?>"><?=get_the_title()?></a></li>
+                                    <?php
+                                }
+                                ?>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="sidebar__red">
-                        <h3>Membership</h3>
-                        <?php
-                        $parent = 362;
-                        $sibs = new WP_Query(array(
-                            'post_type'      => 'page',
-                            'posts_per_page' => -1,
-                            'post_parent'    => $parent,
-                            'order'          => 'ASC',
-                            'orderby'        => 'title'
-                        ));
-                        ?>
-                        <ul>
+                    <div class="gsidebar__blue mb-2">
+                        <div class="h4">GTMA Member Services</div>
+                        <div class="gsidebar__inner">
+                            <p class="mb-2">Here are some of the services available to members</p>
                             <?php
-                            while($sibs->have_posts()) {
-                                $sibs->the_post();
-                                ?>
-                            <li><a href="<?=get_the_permalink()?>"><?=get_the_title()?></a></li>
-                                <?php
-                            }
+                            $parent = 134;
+                            $sibs = new WP_Query(array(
+                                'post_type'      => 'page',
+                                'posts_per_page' => -1,
+                                'post_parent'    => $parent,
+                                'order'          => 'ASC',
+                                'orderby'        => 'title'
+                            ));
                             ?>
-                        </ul>
+                            <ul>
+                                <?php
+                                while($sibs->have_posts()) {
+                                    $sibs->the_post();
+                                    $active = get_the_permalink() == $current_url ? 'active' : '';
+                                    ?>
+                                <li><a class="<?=$active?>" href="<?=get_the_permalink()?>"><?=get_the_title()?></a></li>
+                                    <?php
+                                }
+                                ?>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="sidebar__search">
+                        <div class="h4 mb-3">Search for Products &amp; Services</div>
+                        <div class="gsidebar__inner">
+                            <div class="form">
+                                <input class="form-control" type="text" id="searchInput" autocomplete="off">
+                                <button id="go" class="btn-search">Search</button>
+                                <input type="hidden" name="source" id="sourceInput">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- page-templates/sidebar-page.php
+++ page-templates/sidebar-page.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Sidebar with Siblings
+Template Name: Sidebar with Siblings (BP)
 */
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
@@ -30,9 +30,9 @@ if ($parent == $services->ID) {
 ?>
 <main id="main" class="<?=$class?>">
     <div class="container-xl">
-        <h1><?=get_the_title()?></h1>
         <div class="row">
             <div class="col-md-8">
+                <h1><?=get_the_title()?></h1>
                 <?php
 if ($class == 'partner') {
     ?>
@@ -43,41 +43,44 @@ if ($class == 'partner') {
                 the_content();
 ?>
             </div>
-            <div class="col-md-4 sidebar">
-                <div class="sidebar__inner">
-                    <div class="sidebar__siblings mb-4">
-                        <h3><?=get_field('sidebar_title')?>
-                        </h3>
-                        <?php
-$sibs = new WP_Query(array(
-    'post_type'      => 'page',
-    'posts_per_page' => -1,
-    'post_parent'    => $parent,
-    'order'          => 'ASC',
-    'orderby'        => 'title'
-));
-if ($sibs->have_posts()) {
-    echo '<ul>';
-    while ($sibs->have_posts()) {
-        $sibs->the_post();
-        $active = get_the_ID() == $me ? 'active' : '';
-        ?>
-                        <li><a class="<?=$active?>"
-                                href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
-                        </li>
-                        <?php
-    }
-    echo '</ul>';
-}
-wp_reset_postdata();
-?>
+            <div class="col-md-4">
+                <div class="gsidebar pb-4">
+                    <div class="gsidebar__blue mb-2">
+                        <div class="h4"><?=get_field('sidebar_title')?></div>
+                        <div class="gsidebar__inner">
+                            <?php
+                            $sibs = new WP_Query(array(
+                                'post_type'      => 'page',
+                                'posts_per_page' => -1,
+                                'post_parent'    => $parent,
+                                'order'          => 'ASC',
+                                'orderby'        => 'title'
+                            ));
+                            if ($sibs->have_posts()) {
+                                echo '<ul>';
+                                while ($sibs->have_posts()) {
+                                    $sibs->the_post();
+                                    $active = get_the_ID() == $me ? 'active' : '';
+                                    ?>
+                                                    <li><a class="<?=$active?>"
+                                                            href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
+                                                    </li>
+                                                    <?php
+                                }
+                                echo '</ul>';
+                            }
+                            wp_reset_postdata();
+                            ?>
+                        </div>
                     </div>
-                    <div class="sidebar__search">
-                        <div class="h4 mb-3">Search for Products &amp; Services</div>
-                        <div class="form">
-                            <input class="form-control" type="text" id="searchInput" autocomplete="off">
-                            <button id="go" class="btn-search">Search</button>
-                            <input type="hidden" name="source" id="sourceInput">
+                    <div class="gsidebar__red mb-2">
+                        <div class="h4">Search for Products &amp; Services</div>
+                        <div class="gsidebar__inner">
+                            <div class="form">
+                                <input class="form-control" type="text" id="searchInput" autocomplete="off">
+                                <button id="go" class="btn-search">Search</button>
+                                <input type="hidden" name="source" id="sourceInput">
+                            </div>
                         </div>
                     </div>
                 </div>
