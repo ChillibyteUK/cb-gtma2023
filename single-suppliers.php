9bb91656f841f9d4cda38ec882a6b0d382c25682
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-10-18 10:33:09 +0100

    Update single-suppliers.php

--- single-suppliers.php
+++ single-suppliers.php
@@ -121,8 +121,8 @@ foreach ($blocks as $block) {
     if ($block['blockName'] == 'acf/cb-supplier-assets') {
         $blockTitle = $block['attrs']['data']['type'];
         $navitems[] = $blockTitle;
-        $id = preg_replace('/\\/','-',$blockTitle);
-        $id = acf_slugify($id);
+        // $id = preg_replace('/\\/','-',$blockTitle);
+        $id = cbslugify($blockTitle);
         echo '<a id="' . $id . '" class="anchor"></a>'
         ?>
         <div class="accordion-item" id="a_<?=$id?>" class="anchor">
