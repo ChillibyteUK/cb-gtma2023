635fdc9dd92ab1a4e28dc1725d34d2a41c0870be
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-11-29 09:21:33 +0000

    supplier category styling

--- archive-suppliers.php
+++ archive-suppliers.php
@@ -7,11 +7,27 @@ get_header();
 
     <section class="suppliers">
         <div class="container-xl">
-            <h1>Supplier Specialities</h1>
-            <p>The GTMA supplier directory contains the profiles of companies dedicated to supplying countless industries with the very best services from the manufacturing and engineering sectors. From aerospace and medical, to chemical, defence, and everything in between, the GTMA supplier directory will connect you to the company best suited to your needs.</p>
-            <?php
-            require get_stylesheet_directory() . '/page-templates/blocks/cb_supplier_categories.php';
-            ?>
+            <div class="row">
+                <div class="col-md-9">
+                    <h1>Supplier Specialities</h1>
+                    <p><strong>The GTMA supplier directory contains the profiles of companies dedicated to supplying countless industries with the very best services from the manufacturing and engineering sectors. From aerospace and medical, to chemical, defence, and everything in between, the GTMA supplier directory will connect you to the company best suited to your needs.</strong></p>
+                    <?php
+                    require get_stylesheet_directory() . '/page-templates/blocks/cb_supplier_categories.php';
+                    ?>
+                </div>
+                <div class="col-md-3">
+                    <div class="sidebar__search">
+                        <div class="h5 mb-3">Search Products &amp; Services</div>
+                        <div class="sidebar__inner">
+                            <div class="form">
+                                <input class="form-control" type="text" id="searchInput" autocomplete="off">
+                                <button id="go" class="btn-search"></button>
+                                <input type="hidden" name="source" id="sourceInput">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </main>
--- page-templates/blocks/cb_supplier_categories.php
+++ page-templates/blocks/cb_supplier_categories.php
@@ -1,5 +1,6 @@
 <section class="suppliers py-5">
-    <div class="container-xl">
+    <div class="container-xl px-5 py-4 bg-grey-200">
+        <ul class="cols-lg-2">
         <?php
         $terms = get_terms(array('taxonomy' => 'types', 'hide_empty' => true ));
         foreach ($terms as $t) {
@@ -10,5 +11,6 @@
         <?php
         }
         ?>
+        </ul>
     </div>
 </section>
