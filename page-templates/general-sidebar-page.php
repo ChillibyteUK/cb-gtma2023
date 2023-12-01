987e78e276ccbd2a63a695638b96567c90d97ba0
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2023-12-01 10:16:13 +0000

    Update general-sidebar-page.php

--- page-templates/general-sidebar-page.php
+++ page-templates/general-sidebar-page.php
@@ -9,14 +9,22 @@ $class = $block['className'] ?? null;
 
 get_header();
 global $wp;
-$current_url = home_url( add_query_arg( array(), $wp->request ) ) . '/';
+$current_url = home_url(add_query_arg(array(), $wp->request)) . '/';
+$blocks = parse_blocks($content);
 ?>
 <main id="main" class="<?=$class?>">
     <div class="container-xl">
         <div class="row">
             <div class="col-md-9">
+                <?php
+                foreach ($blocks as $block) {
+                    echo $block['blockName'];
+                    // if ($block['blockName'] == 'acf') {
+                    // }
+                }
+?>
                 <h1><?=get_the_title()?></h1>
-                <?=apply_filters('the_content',get_the_content())?>
+                <?=apply_filters('the_content', get_the_content())?>
             </div>
             <div class="col-md-3">
                 <div class="sidebar pb-4">
@@ -24,28 +32,32 @@ $current_url = home_url( add_query_arg( array(), $wp->request ) ) . '/';
                         <div class="h5">Membership</div>
                         <div class="sidebar__inner">
                             <?php
-                            // $parent = 362;
-                            $parent = get_page_by_path( 'membership' );
-                            $sibs = new WP_Query(array(
-                                'post_type'      => 'page',
-                                'posts_per_page' => -1,
-                                'post_parent'    => $parent->ID,
-                                'order'          => 'ASC',
-                                'orderby'        => 'title'
-                            ));
-                            $active = get_the_permalink($parent) == $current_url ? 'active' : '';
-                            ?>
+            // $parent = 362;
+            $parent = get_page_by_path('membership');
+$sibs = new WP_Query(array(
+    'post_type'      => 'page',
+    'posts_per_page' => -1,
+    'post_parent'    => $parent->ID,
+    'order'          => 'ASC',
+    'orderby'        => 'title'
+));
+$active = get_the_permalink($parent) == $current_url ? 'active' : '';
+?>
                             <ul>
-                                <li><a class="<?=$active?>" href="<?=get_the_permalink($parent)?>"><?=get_the_title($parent)?></a></li>
+                                <li><a class="<?=$active?>"
+                                        href="<?=get_the_permalink($parent)?>"><?=get_the_title($parent)?></a>
+                                </li>
                                 <?php
-                                while($sibs->have_posts()) {
-                                    $sibs->the_post();
-                                    $active = get_the_permalink() == $current_url ? 'active' : '';
-                                    ?>
-                                <li><a class="<?=$active?>" href="<?=get_the_permalink()?>"><?=get_the_title()?></a></li>
-                                    <?php
-                                }
-                                ?>
+    while($sibs->have_posts()) {
+        $sibs->the_post();
+        $active = get_the_permalink() == $current_url ? 'active' : '';
+        ?>
+                                <li><a class="<?=$active?>"
+                                        href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
+                                </li>
+                                <?php
+    }
+?>
                             </ul>
                         </div>
                     </div>
@@ -55,24 +67,26 @@ $current_url = home_url( add_query_arg( array(), $wp->request ) ) . '/';
                             <p class="mb-2">Here are some of the services available to members</p>
                             <?php
                             $parent = 134;
-                            $sibs = new WP_Query(array(
-                                'post_type'      => 'page',
-                                'posts_per_page' => -1,
-                                'post_parent'    => $parent,
-                                'order'          => 'ASC',
-                                'orderby'        => 'title'
-                            ));
-                            ?>
+$sibs = new WP_Query(array(
+    'post_type'      => 'page',
+    'posts_per_page' => -1,
+    'post_parent'    => $parent,
+    'order'          => 'ASC',
+    'orderby'        => 'title'
+));
+?>
                             <ul>
                                 <?php
-                                while($sibs->have_posts()) {
-                                    $sibs->the_post();
-                                    $active = get_the_permalink() == $current_url ? 'active' : '';
-                                    ?>
-                                <li><a class="<?=$active?>" href="<?=get_the_permalink()?>"><?=get_the_title()?></a></li>
-                                    <?php
-                                }
-                                ?>
+    while($sibs->have_posts()) {
+        $sibs->the_post();
+        $active = get_the_permalink() == $current_url ? 'active' : '';
+        ?>
+                                <li><a class="<?=$active?>"
+                                        href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
+                                </li>
+                                <?php
+    }
+?>
                             </ul>
                         </div>
                     </div>
