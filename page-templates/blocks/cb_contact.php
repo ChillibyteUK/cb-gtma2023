3b8a67a9e44e0b6f1e16b1f078a574386954e710
CheechRockwizard <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-05 16:38:16 +0000

    Update cb_contact.php

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -19,19 +19,19 @@
                 <h3>Email</h3>
 
                 <div class="row mb-4">
-                    <div class="col-md-6">
-                        <h4 class="h5 mb-2">Technical &amp; Supply Chain</h4>
+                    <div class="col-md-6 text-center">
+                        <h4 class="h5 fw-600 mb-2">Technical &amp; Supply Chain</h4>
                         <img src="<?=get_stylesheet_directory_uri()?>/img/alan.jpg"
-                            alt="Alan Arthur" class="d-block mb-2 rounded-circle w-75">
+                            alt="Alan Arthur" class="d-block mb-2 rounded-circle w-50">
                         <p><strong>Alan Arthur, CEO</strong></p>
                         <a href="mailto:alan@gtma.co.uk"><i class="fa-solid fa-envelope fs-500 pe-2"></i></a>
                         <a href="https://www.linkedin.com/in/alan-arthur-41550426/" target="_blank"><i
                                 class="fa-brands fa-linkedin-in fs-500"></i></a>
                     </div>
-                    <div class="col-md-6">
-                        <h4 class="h5 mb-2">Membership &amp; Other Enquiries</h4>
+                    <div class="col-md-6 text-center">
+                        <h4 class="h5 fw-600 mb-2">Membership &amp; Other Enquiries</h4>
                         <img src="<?=get_stylesheet_directory_uri()?>/img/david.jpg"
-                            alt="David Beattie" class="d-block mb-2 rounded-circle w-75">
+                            alt="David Beattie" class="d-block mb-2 rounded-circle w-50">
                         <p><strong>David Beattie, General Manager</strong></p>
                         <a href="mailto:david@gtma.co.uk"><i class="fa-solid fa-envelope fs-500 pe-2"></i></a>
                         <a href="https://www.linkedin.com/in/david-beattie-1955a952/" target="_blank"><i
