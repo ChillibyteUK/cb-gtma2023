20df99646bb542a2b9227d261140e1679fba2eb7
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-06 12:35:25 +0000

    Update cb-taxonomies.php

--- inc/cb-taxonomies.php
+++ inc/cb-taxonomies.php
@@ -14,7 +14,7 @@ function cb_register_taxes()
         ],
         "public" => true,
         "publicly_queryable" => true,
-        "hierarchical" => false,
+        "hierarchical" => true,
         "show_ui" => true,
         "show_in_menu" => true,
         "show_in_nav_menus" => true,
