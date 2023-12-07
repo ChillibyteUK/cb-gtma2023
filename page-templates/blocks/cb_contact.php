73f5816aa98ad1a2e134c656f86228506cdc90ae
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-07 16:20:55 +0000

    new alan

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -2,28 +2,22 @@
     <div class="container-xl">
         <div class="row mb-4">
             <div class="col-md-6">
-                <h3>Address</h3>
-                <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span>
+                <h3>Contact Details</h3>
+                <ul class="fa-ul mb-5">
+                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span>
                         <?=str_replace(',', ',<br>', get_field('contact_address', 'options'))?>
                     </li>
-                </ul>
-
-                <h3>Telephone</h3>
-                <ul class="fa-ul">
                     <li><span class="fa-li"><i class="fa-solid fa-phone"></i></span> <a
                             href="tel:<?=parse_phone(get_field('contact_phone', 'options'))?>"><?=get_field('contact_phone', 'options')?></a>
                     </li>
                 </ul>
 
-                <h3>Email</h3>
-
-                <div class="row mb-4">
+                <div class="row mb-5">
                     <div class="col-md-6 text-center">
                         <h4 class="h5 fw-600 mb-2">Technical &amp; Supply Chain Enquiries</h4>
                         <img src="<?=get_stylesheet_directory_uri()?>/img/alan.jpg"
                             alt="Alan Arthur" class="d-block mb-2 rounded-circle w-50 mx-auto">
-                        <p class="mb-1"><strong>Alan Arthur, CEO</strong></p>
+                        <p class="mb-1"><strong>Alan Arthur</strong><br>CEO</p>
                         <a href="mailto:alan@gtma.co.uk"><i class="fa-solid fa-envelope fs-500 pe-2"></i></a>
                         <a href="https://www.linkedin.com/in/alan-arthur-41550426/" target="_blank"><i
                                 class="fa-brands fa-linkedin-in fs-500"></i></a>
@@ -32,7 +26,7 @@
                         <h4 class="h5 fw-600 mb-2">Membership &amp; Other Enquiries</h4>
                         <img src="<?=get_stylesheet_directory_uri()?>/img/david.jpg"
                             alt="David Beattie" class="d-block mb-2 rounded-circle w-50 mx-auto">
-                        <p class="mb-1"><strong>David Beattie, General Manager</strong></p>
+                        <p class="mb-1"><strong>David Beattie</strong><br>General Manager</p>
                         <a href="mailto:david@gtma.co.uk"><i class="fa-solid fa-envelope fs-500 pe-2"></i></a>
                         <a href="https://www.linkedin.com/in/david-beattie-1955a952/" target="_blank"><i
                                 class="fa-brands fa-linkedin-in fs-500"></i></a>
