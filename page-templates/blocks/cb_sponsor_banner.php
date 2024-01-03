4814b93550882939e865353a41594771475b6d17
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-01-03 17:19:51 +0000

    sponsor banner

--- page-templates/blocks/cb_sponsor_banner.php
+++ page-templates/blocks/cb_sponsor_banner.php
@@ -7,7 +7,7 @@ if ($until >= $today) {
 <section class="sponsor mb-4">
     <div class="container-xl">
         <a href="<?=get_the_permalink($sponsor)?>" class="sponsor__card">
-            <div class="sponsor__title">This content is sponsored by <strong><?=get_the_title($sponsor)?></strong>.</div>
+            <div class="sponsor__title">This content is sponsored by <span><?=get_the_title($sponsor)?></span>.</div>
             <div class="sponsor__inner">
                 <div class="sponsor__words">
                     <p class="fs-200"><?=wp_trim_words(get_the_content(null, false, $sponsor),30)?></p>
