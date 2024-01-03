020e8c522d8cb5890d9181c969a0c7516a2ac8bc
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-01-03 11:18:49 +0000

    Update index.php

--- index.php
+++ index.php
@@ -17,10 +17,10 @@ get_header();
         </div>
         <?php
         // $cats = get_categories(array('exclude' => array(32)));
-        $cats = get_terms(array('taxonomy'=>'sectors', 'hide_empty' => false));
+        $cats = get_terms(array('taxonomy'=>'categories', 'hide_empty' => false));
 echo '<a class="me-2 mb-2 filter-button active" href="/news/">All News</a>';
 foreach($cats as $c) {
-    echo '<a class="me-2 mb-2 filter-button" href="/sector/' . $c->slug . '/">' . $c->name . '</a>';
+    echo '<a class="me-2 mb-2 filter-button" href="/news/category/' . $c->slug . '/">' . $c->name . '</a>';
 }
 
 /*
