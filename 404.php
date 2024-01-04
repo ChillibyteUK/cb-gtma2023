5c1d3da383ed19497b8bca738bff22619c6ad923
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-01-04 14:53:38 +0000

    added 404

--- 404.php
+++ 404.php
@@ -5,27 +5,20 @@ defined('ABSPATH') || exit;
 get_header();
 ?>
 <main id="main" class="padding-top">
-<?php
-$bg = wp_get_attachment_image_url( get_field('hero_image','options'), 'full' );
-?>    
-<!-- hero -->
-<section id="hero" class="hero d-flex align-items-center hero--default mb-0">
-    <div class="overlay"></div>
-    <div class="hero__inner container-xl">
-        <div class="row h-100">
-            <div class="col-lg-6 hero__content d-flex flex-column justify-content-center order-2 order-lg-1 py-5" data-aos="fade">
-                <h1 class="mb-4">404 - Page Not Found</h1>
-                <div class="hero__content fs-5 mb-4">We can't seem to find the page you're looking for</div>
-                <div class="hero__cta">
-                    <a class="btn btn-default--secondary mb-4" href="/">Return to Homepage</a>
+    <!-- hero -->
+    <section class="d-flex align-items-center">
+        <div class="container-xl">
+            <div class="row h-100">
+                <div class="col-lg-6 d-flex flex-column justify-content-center order-2 order-lg-1 py-5">
+                    <h1 class="mb-4">404 - Page Not Found</h1>
+                    <div class="hero__content fs-5 mb-4">We can't seem to find the page you're looking for</div>
+                    <div class="hero__cta">
+                        <a class="btn btn-primary mb-4" href="/">Return to Homepage</a>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-6 hero__image order-1 order-lg-2" style="background-image:url(<?=$bg?>)">
-            </div>
         </div>
-    </div>
-    <div class="overlay--bottom"></div>
-</section>
+    </section>
 </main>
 <?php
 get_footer();
