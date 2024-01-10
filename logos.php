20983d80869c7dbd618ee4e752c7365630235cb8
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-01-10 17:12:17 +0000

    Update logos.php

--- logos.php
+++ logos.php
@@ -19,11 +19,13 @@ $q = new WP_Query(array(
 
 while ($q->have_posts()) {
     $q->the_post();
+    $alt_text = get_post_meta(get_field('supplier_logo',get_the_ID()), '_wp_attachment_image_alt', true) ?: 'NOT SET';
     ?>
     <div>
         <a href="<?=get_the_permalink()?>" target="_blank">
             <img src="<?=wp_get_attachment_image_url(get_field('supplier_logo', get_the_ID()), 'full')?>"><br>
             <?=get_the_title()?>
+            <pre><?=esc_html($alt_text)?></pre>
         </a>
     <hr>
     </div>
