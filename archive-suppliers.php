f081998cc7f20bc8f7476b245c75c54fa46b365b
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-01-17 12:41:32 +0000

    Update archive-suppliers.php

--- archive-suppliers.php
+++ archive-suppliers.php
@@ -5,32 +5,93 @@ get_header();
 ?>
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <main id="main" class="supplier-archive">
-
-    <section class="suppliers">
-        <div class="container-xl">
-            <div class="row">
-                <div class="col-md-9">
+<div class="container-xl">
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-md-9">
+                <section class="suppliers">
                     <h1>Supplier Specialities</h1>
                     <p><strong>The GTMA supplier directory contains the profiles of companies dedicated to supplying countless industries with the very best services from the manufacturing and engineering sectors. From aerospace and medical, to chemical, defence, and everything in between, the GTMA supplier directory will connect you to the company best suited to your needs.</strong></p>
                     <?php
                     require get_stylesheet_directory() . '/page-templates/blocks/cb_supplier_categories.php';
                     ?>
+                </section>
+            </div>
+            <div class="col-md-3">
+                <div class="sidebar mb-2">
+                    <div class="sidebar__red mb-2">
+                    <div class="h5">Membership</div>
+                    <div class="sidebar__inner">
+                        <?php
+            // $parent = 362;
+            $parent = get_page_by_path('membership');
+$sibs = new WP_Query(array(
+    'post_type'      => 'page',
+    'posts_per_page' => -1,
+    'post_parent'    => $parent->ID,
+    'order'          => 'ASC',
+    'orderby'        => 'title'
+));
+?>
+                        <ul>
+                            <li><a class=""
+                                    href="<?=get_the_permalink($parent)?>"><?=get_the_title($parent)?></a>
+                            </li>
+                            <?php
+                            while($sibs->have_posts()) {
+                                $sibs->the_post();
+                                ?>
+                            <li><a class="" href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
+                            </li>
+                            <?php
+                            }
+                            ?>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="sidebar sidebar__search">
-                        <div class="h5 mb-3">Search Products & Services</div>
-                        <div class="sidebar__inner">
-                            <div class="form">
-                                <input class="form-control" type="text" id="searchInput" autocomplete="off">
-                                <button id="go" class="btn-search"></button>
-                                <input type="hidden" name="source" id="sourceInput">
-                            </div>
+                <div class="sidebar__blue mb-2">
+                    <div class="h5">Member Services</div>
+                    <div class="sidebar__inner">
+                        <p class="mb-2">Here are some of the services available to members</p>
+                        <?php
+                        $parent = 134;
+                        $sibs = new WP_Query(array(
+                            'post_type'      => 'page',
+                            'posts_per_page' => -1,
+                            'post_parent'    => $parent,
+                            'order'          => 'ASC',
+                            'orderby'        => 'title'
+                        ));
+                        ?>
+                        <ul>
+                            <?php
+                            while($sibs->have_posts()) {
+                                $sibs->the_post();
+                                ?>
+                            <li><a class=""
+                                    href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
+                            </li>
+                            <?php
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+                <div class="sidebar__search">
+                    <div class="h5 mb-3">Search Products & Services</div>
+                    <div class="sidebar__inner">
+                        <div class="form">
+                            <input class="form-control" type="text" id="searchInput" autocomplete="off">
+                            <button id="go" class="btn-search"></button>
+                            <input type="hidden" name="source" id="sourceInput">
                         </div>
                     </div>
                 </div>
+
             </div>
-        </div>
-    </section>
+        </div> <!-- .col -->
+    </div>
+    </div>
 </main>
 
 <?php
