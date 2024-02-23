d1fb0f79e79b0d8fd4ec3df5925d96f834dc6720
CheechRockwizard <83024977+ChillibyteUK@users.noreply.github.com>
2024-02-23 08:58:49 +0000

    Update sponsor-sidebar-page.php

--- page-templates/sponsor-sidebar-page.php
+++ page-templates/sponsor-sidebar-page.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Sponsor Sidebar Page
+Template Name: Full-hero Sponsor Sidebar Page
 */
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
@@ -15,17 +15,20 @@ $blocks = parse_blocks($content);
 ?>
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <main id="main" class="<?=$class?>">
-    <div class="container-xl">
-        <div class="row">
-            <div class="col-md-9">
-                <?php
-                $hasHero = false;
+    <?php
+$hasHero = false;
 foreach ($blocks as $block) {
     // echo $block['blockName'];
     if ($block['blockName'] == 'acf/cb-hero') {
         $hasHero = true;
+        echo render_block($block);
     }
 }
+?>
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-md-9">
+                <?php
 if ($hasHero === false) {
     echo '<h1>' . get_the_title() . '</h1>';
 }
@@ -33,7 +36,9 @@ if ($hasHero === false) {
 $banner = array();
 
 foreach ($blocks as $block) {
-    if ($block['blockName'] == 'acf/cb-sponsor-banner') {
+    if ($block['blockName'] == 'acf/cb-hero') {
+        continue;
+    } elseif ($block['blockName'] == 'acf/cb-sponsor-banner') {
         if ($block['attrs']['data']['aspect'] == 'Banner') {
             echo render_block($block);
         } elseif ($block['attrs']['data']['aspect'] == 'Skyscraper') {
@@ -49,78 +54,6 @@ foreach ($blocks as $block) {
             </div>
             <div class="col-md-3">
                 <div class="sidebar pb-4">
-                    <div class="sidebar__red mb-2">
-                        <div class="h5">Membership</div>
-                        <div class="sidebar__inner">
-                            <?php
-            // $parent = 362;
-            $parent = get_page_by_path('membership');
-$sibs = new WP_Query(array(
-    'post_type'      => 'page',
-    'posts_per_page' => -1,
-    'post_parent'    => $parent->ID,
-    'order'          => 'ASC',
-    'orderby'        => 'title'
-));
-$active = get_the_permalink($parent) == $current_url ? 'active' : '';
-?>
-                            <ul>
-                                <li><a class="<?=$active?>"
-                                        href="<?=get_the_permalink($parent)?>"><?=get_the_title($parent)?></a>
-                                </li>
-                                <?php
-    while($sibs->have_posts()) {
-        $sibs->the_post();
-        $active = get_the_permalink() == $current_url ? 'active' : '';
-        ?>
-                                <li><a class="<?=$active?>"
-                                        href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
-                                </li>
-                                <?php
-    }
-?>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="sidebar__blue mb-2">
-                        <div class="h5">Member Services</div>
-                        <div class="sidebar__inner">
-                            <p class="mb-2">Here are some of the services available to members</p>
-                            <?php
-                            $parent = 134;
-$sibs = new WP_Query(array(
-    'post_type'      => 'page',
-    'posts_per_page' => -1,
-    'post_parent'    => $parent,
-    'order'          => 'ASC',
-    'orderby'        => 'title'
-));
-?>
-                            <ul>
-                                <?php
-    while($sibs->have_posts()) {
-        $sibs->the_post();
-        $active = get_the_permalink() == $current_url ? 'active' : '';
-        ?>
-                                <li><a class="<?=$active?>"
-                                        href="<?=get_the_permalink()?>"><?=get_the_title()?></a>
-                                </li>
-                                <?php
-    }
-?>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="sidebar__search">
-                        <div class="h5 mb-3">Search Products & Services</div>
-                        <div class="sidebar__inner">
-                            <div class="form">
-                                <input class="form-control" type="text" id="searchInput" autocomplete="off">
-                                <button id="go" class="btn-search"></button>
-                                <input type="hidden" name="source" id="sourceInput">
-                            </div>
-                        </div>
-                    </div>
                     <?php
 if ($banner ?? null) {
     echo render_block($banner);
@@ -132,28 +65,5 @@ if ($banner ?? null) {
     </div>
 </main>
 <?php
-add_action('wp_footer', function () {
-    ?>
-<script>
-    <?php
-$supp = get_posts(array(
-    'post_type' => 'suppliers',
-    'numberposts' => -1
-));
-
-    $suppliers = array();
-
-    foreach ($supp as $p) {
-        $t = strip_crud($p->post_title);
-        $suppliers[$t] = $p->post_name;
-    }
-    ?>
-    var slugList = [ <?=json_encode($suppliers)?> ];
-</script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<script src="<?=get_stylesheet_directory_uri()?>/js/search.js"></script>
-<?php
-}, 9999);
 get_footer();
 ?>
