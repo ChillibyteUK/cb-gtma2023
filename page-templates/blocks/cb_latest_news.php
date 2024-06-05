6ae41e3e5605c214895f61f8fb1436e72a01a5e1
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-05 11:50:44 +0100

    Update cb_latest_news.php

--- page-templates/blocks/cb_latest_news.php
+++ page-templates/blocks/cb_latest_news.php
@@ -11,6 +11,9 @@ $q = new WP_Query(array(
     <?php
     $n = 1;
 while ($q->have_posts()) {
+    if ($n > 3) {
+        continue;
+    }
     $q->the_post();
     $img = get_the_post_thumbnail_url(get_the_ID(), 'large');
     if (!$img) {
