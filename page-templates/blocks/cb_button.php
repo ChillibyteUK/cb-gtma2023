17de0149f9bba909454698ed7e442c55e5ea2eb1
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-16 10:29:07 +0100

    Update cb_button.php

--- page-templates/blocks/cb_button.php
+++ page-templates/blocks/cb_button.php
@@ -3,11 +3,7 @@ $l = get_field('link');
 
 $classes = $block['className'] ?? null;
 
-// var_dump($block);
-
 $background_color = !empty($block['backgroundColor']) ? $block['backgroundColor'] : '';
-
-$classes = '';
 
 if ($background_color) {
     $classes .= ' btn-' . $background_color;
