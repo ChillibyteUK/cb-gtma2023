7b9f232703e5dd60e82c047cb2f76517997d8f38
Matthew Pont <matthew.pont@gmail.com>
2024-09-19 11:15:14 +0100

    Update supplier-find.php

--- supplier-find.php
+++ supplier-find.php
@@ -1,11 +1,6 @@
 <?php
 require_once('../../../wp-config.php');
 
-function encodeURIComponent($str) {
-    $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
-    return strtr(rawurlencode($str), $revert);
-}
-
 $args = array(
     'post_type' => 'suppliers',
     'post_status' => 'publish',
@@ -18,13 +13,13 @@ $query = new WP_Query($args);
 if ($query->have_posts()) {
     while ($query->have_posts()) {
         $query->the_post();
-        echo "Found:<br>" . encodeURIComponent($_GET['name']) . "<br>" . get_the_permalink();
+        echo "Found:<br>" . urlencode($_GET['name']) . "<br>" . get_the_permalink();
         //header("Location: " . get_the_permalink());
 		//die();
     }
 } else {
     //Page not found
-    echo "Issue for:<br>" . encodeURIComponent($_GET['name']);
+    echo "Issue for:<br>" . urlencode($_GET['name']);
     //header("Location: /404/");
 	//die();
 }
