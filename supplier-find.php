d72316e8847091f2b65c3ed4973cfbfa72c58c56
Matthew Pont <matthew.pont@gmail.com>
2024-09-19 11:22:35 +0100

    Update supplier-find.php

--- supplier-find.php
+++ supplier-find.php
@@ -1,6 +1,15 @@
 <?php
 require_once('../../../wp-config.php');
 
+function get_post_by_name(string $name, string $post_type = "post") {
+    $query = new WP_Query([
+        "post_type" => $post_type,
+        "name" => $name
+    ]);
+
+    return $query->have_posts() ? reset($query->posts) : null;
+}
+
 $search = get_post_by_name(urldecode($_GET['name']), "suppliers");
 if ( $search->ID ) {
     header("Location: " . get_the_permalink($search->ID));
