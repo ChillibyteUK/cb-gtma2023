d2d6bc8edfe17cd7fe58ca07142c35ed5576f8da
Matthew Pont <matthew.pont@gmail.com>
2024-11-22 16:03:19 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -129,3 +129,8 @@ endif;
 
 return ob_get_clean();
 }
+
+function wpb_remove_schedule_delete() {
+    remove_action( 'wp_scheduled_delete', 'wp_scheduled_delete' );
+}
+add_action( 'init', 'wpb_remove_schedule_delete' );
