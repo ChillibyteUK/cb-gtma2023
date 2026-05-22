6e7e6828a2bcceef539dd5ddc3db21311d2e2ce6
Matthew Pont <matthew.pont@gmail.com>
2026-05-22 13:35:02 +0100

    Multi Testimonial

--- page-templates/blocks/cb_multi_testimonial.php
+++ page-templates/blocks/cb_multi_testimonial.php
@@ -1,21 +1,88 @@
 <?php
 $classes = $block['className'] ?? 'mb-5';
-$alt = get_post_meta(get_field('logo'), '_wp_attachment_image_alt', true); // Get alt text
+
+/**
+ * Enqueue Slick only when this block is used
+ */
+wp_enqueue_style(
+    'slick-css',
+    'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css',
+    [],
+    '1.8.1'
+);
+
+wp_enqueue_style(
+    'slick-theme-css',
+    'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css',
+    ['slick-css'],
+    '1.8.1'
+);
+
+wp_enqueue_script(
+    'slick-js',
+    'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js',
+    ['jquery'],
+    '1.8.1',
+    true
+);
+
+$testimonials = get_field('testimonials');
 ?>
-<section class="single_testimonial <?=$classes?>">
+
+<?php if ($testimonials): ?>
+<section class="single_testimonial container-xl my-5 <?php echo esc_attr($classes); ?>">
     <div class="container-xl">
-        <div class="row">
-            <div class="col-md-10 order-2 order-md-1">
-                <div class="single_testimonial__content mb-3">
-                    <?=get_field('testimonial_content')?>
+
+        <div class="multi_testimonials__slider">
+            <?php foreach ($testimonials as $testimonial): 
+                $content = $testimonial['testimonial_content'] ?? '';
+                $attribution = $testimonial['testimonial_attribution'] ?? '';
+                $logo_id = $testimonial['logo'] ?? '';
+                $alt = $logo_id ? get_post_meta($logo_id, '_wp_attachment_image_alt', true) : '';
+            ?>
+                <div class="multi_testimonials__slide">
+                    <div class="row align-items-center">
+                        <div class="col-md-10 order-2 order-md-1">
+                            <div class="single_testimonial__content mb-3">
+                                <?php echo wp_kses_post($content); ?>
+                            </div>
+
+                            <?php if ($attribution): ?>
+                                <div class="single_testimonial__attribution">
+                                    <?php echo esc_html($attribution); ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+
+                        <?php if ($logo_id): ?>
+                            <div class="col-md-2 order-1 order-md-2">
+                                <?php echo wp_get_attachment_image($logo_id, 'large', false, [
+                                    'alt' => esc_attr($alt),
+                                ]); ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
                 </div>
-                <div class="single_testimonial__attribution">
-                    <?=get_field('testimonial_attribution')?>
-                </div>
-            </div>
-            <div class="col-md-2 order-1 order-md-2">
-                <img src="<?=wp_get_attachment_image_url(get_field('logo'),'large')?>" alt="<?php echo $alt; ?>">
-            </div>
+            <?php endforeach; ?>
         </div>
+
     </div>
 </section>
+<?php endif; ?>
+<?php
+wp_add_inline_script('slick-js', "
+    jQuery(function($){
+        $('.multi_testimonials__slider').not('.slick-initialized').slick({
+            dots: true,
+            arrows: false,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 6000,
+            speed: 400,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: true
+        });
+    });
+");
+?>
