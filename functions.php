7c6b0dc27ec79b400193af1495333556ee903e55
Matthew Pont <matthew.pont@gmail.com>
2026-06-24 15:21:28 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -176,3 +176,34 @@ function stats_bar( $atts ){
     return ob_get_clean();
 }
 add_shortcode('stats_bar', 'stats_bar');
+
+add_action('wp_head', function () {
+    ?>
+    <!-- Leadinfo tracking code -->
+    <script>
+    (function(l,e,a,d,i,n,f,o){
+        if(!l[i]){
+            l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[];
+            l.GlobalLeadinfoNamespace.push(i);
+            l[i]=function(){
+                (l[i].q=l[i].q||[]).push(arguments);
+            };
+            l[i].t=l[i].t||n;
+            l[i].q=l[i].q||[];
+            o=e.createElement(a);
+            f=e.getElementsByTagName(a)[0];
+            o.async=1;
+            o.src=d;
+            f.parentNode.insertBefore(o,f);
+        }
+    }(
+        window,
+        document,
+        'script',
+        'https://cdn.leadinfo.net/ping.js',
+        'leadinfo',
+        'LI-695BC7F4BC5B9'
+    ));
+    </script>
+    <?php
+}, 100);
